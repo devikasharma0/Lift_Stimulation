17916437f4d6311fd69d5f201a197c39f6697914
devikasharma0 <89690020+devikasharma0@users.noreply.github.com>
2024-09-14 14:17:11 +0530

    updates

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const simulation = document.getElementById("Simulation");
   const lift_num = document.getElementById("lift_num");
   const floor_num = document.getElementById("floor_num");
   const form = document.getElementById("form");
   const back = document.getElementById("back");
   back.style.display = "none";
+  back.style.width = "60px";
+
   form.addEventListener("submit", (e) => {
     e.preventDefault(); //removing default refresh behaviour of form so the vlaue does not get lost
     const lift_value = lift_num.value;
@@ -14,9 +17,52 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     form.classList.add("hidden");
     back.style.display = "block";
+    createLifts(lift_value);
+    createFloors(floor_value);
   });
 
   back.addEventListener("click", () => {
     form.classList.remove("hidden");
+    simulation.innerHTML = "";
+    back.style.display = "none";
   });
+  
+  function createLifts(lift_value){
+    for(let i = 0; i<lift_value; i++){
+      const lift = document.createElement("div");
+      lift.style.height = "100px";
+      lift.style.width = "70px";
+      lift.style.backgroundColor = "red";
+      lift.style.position = "absolute";
+      lift.style.left = `${100*(i+1)}px`;
+      lift.style.bottom = "2px";
+      simulation.appendChild(lift);
+    }
+  }
+  function createFloors(floor_value) {
+    for(let i = 0; i <floor_value; i++){
+      const floorDiv = document.createElement("div");
+      const upButton = document.createElement("button");
+      const downButton = document.createElement("button");
+      const textDiv = document.createElement("p")
+      textDiv.innerText = `floor ${floor_value-i-1}` //to print value of i 
+      floorDiv.style.height = "150px"
+      floorDiv.style.display = "grid"
+      upButton.style.width = "60px"
+      downButton.style.width = "60px"
+      floorDiv.style.width = "100%"
+      floorDiv.style.borderBottom = "2px solid black"
+      floorDiv.className = "floordiv";
+      upButton.className = "upbutton";
+      downButton.className = "downbutton";
+      upButton.innerText = "up";
+      downButton.innerText = "down";
+      floorDiv.appendChild(textDiv);
+      floorDiv.appendChild(upButton);
+      floorDiv.appendChild(downButton);
+      simulation.appendChild(floorDiv);
+
+    }
+  }
+
 });
