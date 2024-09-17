52774c17b649583d708e78ba246f2b4250f3d721
devikasharma0 <89690020+devikasharma0@users.noreply.github.com>
2024-09-17 18:15:48 +0530

    button conditions

--- main.js
+++ main.js
@@ -28,43 +28,47 @@ document.addEventListener("DOMContentLoaded", () => {
     container.style.display = "flex";
     simulation.innerHTML = "";
   });
-  
-  function createLifts(lift_value){
-    for(let i = 0; i<lift_value; i++){
+
+  function createLifts(lift_value) {
+    for (let i = 0; i < lift_value; i++) {
       const lift = document.createElement("div");
       lift.style.height = "100px";
       lift.style.width = "70px";
       lift.style.backgroundColor = "#69b0fc";
       lift.style.position = "absolute";
-      lift.style.left = `${100*(i+1)}px`;
+      lift.style.left = `${100 * (i + 1)}px`;
       lift.style.bottom = "2px";
       simulation.appendChild(lift);
     }
   }
   function createFloors(floor_value) {
-    for(let i = 0; i <floor_value; i++){
+    for (let i = 0; i < floor_value; i++) {
       const floorDiv = document.createElement("div");
       const upButton = document.createElement("button");
       const downButton = document.createElement("button");
-      const textDiv = document.createElement("p")
-      textDiv.innerText = `Floor ${floor_value-i-1}` //to print value of i 
-      floorDiv.style.height = "150px"
-      floorDiv.style.display = "grid"
-      upButton.style.width = "60px"
-      downButton.style.width = "60px"
-      floorDiv.style.width = "100%"
-      floorDiv.style.borderBottom = "2px solid black"
+      const textDiv = document.createElement("p");
+      textDiv.innerText = `Floor ${floor_value - i - 1}`; //to print value of i
+      floorDiv.style.height = "150px";
+      floorDiv.style.display = "grid";
+      upButton.style.width = "60px";
+      downButton.style.width = "60px";
+      floorDiv.style.width = "100%";
+      floorDiv.style.borderBottom = "2px solid black";
       floorDiv.className = "floordiv";
       upButton.className = "upbutton";
       downButton.className = "downbutton";
       upButton.innerText = "Up";
       downButton.innerText = "Down";
       floorDiv.appendChild(textDiv);
-      floorDiv.appendChild(upButton);
-      floorDiv.appendChild(downButton);
+      if (i === 0) {
+        floorDiv.appendChild(downButton);
+      } else if (i === floor_value - 1) {
+        floorDiv.appendChild(upButton);
+      } else {
+        floorDiv.appendChild(upButton);
+        floorDiv.appendChild(downButton);
+      }
       simulation.appendChild(floorDiv);
-
     }
   }
-
 });
